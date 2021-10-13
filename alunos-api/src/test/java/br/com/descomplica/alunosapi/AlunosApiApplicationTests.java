195adccdca1fb9e9ebe87922211a65153c0e89fe
Gabriel Frael <3rd.gabriel.frael>
2021-10-13 18:11:12 -0300

    Arquivos de configuração.

---
+++ alunos-api/src/test/java/br/com/descomplica/alunosapi/AlunosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.descomplica.alunosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
